3794c1c23065585dbc5e4150e676b237a344cf0b
networkop <mmkashin@gmail.com>
2020-10-01 18:46:19 +0100

    Added weavenet

--- .mk/plugins.mk
+++ .mk/plugins.mk
@@ -4,15 +4,15 @@ delete-kindnet:
 flannel: delete-kindnet preload-cni-image
 	helm upgrade --namespace flux -f flux-values.yml --set git.branch=flannel flux fluxcd/flux
 
+weave: delete-kindnet 
+	helm upgrade --namespace flux -f flux-values.yml --set git.branch=weave flux fluxcd/flux
 
 nuke-all-pods:
 	kubectl delete --all pods --all-namespaces	
 
-
 preload-cni-image:
 	docker build -t networkop.co.uk/cni-install cni-installer
 	kind load docker-image networkop.co.uk/cni-install:latest --name k8s-guide
-
 
 flush-nat:
 	docker exec -it k8s-guide-control-plane iptables --table nat --flush
