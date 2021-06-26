9ede596c0eb7baaf008cdce5365c0d2417b2dfd5
networkop <mmkashin@gmail.com>
2021-06-26 18:31:46 +0100

    Added ipvs

--- .mk/ipvs.mk
+++ .mk/ipvs.mk
@@ -1,6 +1,7 @@
 ipvs: flux-init-wait
 	helm upgrade --namespace flux -f flux-values.yml --set git.branch=ipvs flux fluxcd/flux
 
-
+kube-proxy-logs:
+	kubectl logs -l k8s-app=kube-proxy -n kube-system
 
 
