8b5162b4fd34b6252ff07af4c5f14d59cb35a5ce
networkop <mmkashin@gmail.com>
2021-08-28 10:24:30 +0100

    Added cilium egress nat gw

--- .mk/ingress.mk
+++ .mk/ingress.mk
@@ -1,3 +1,6 @@
+CONTROL_PLANE_NODE_IP := $(shell docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' k8s-guide-control-plane)
+KIND_BRIDGE_SUBNET := $(shell docker network inspect kind --format='{{(index .IPAM.Config 0).Subnet}}'e)
+
 ingress-nginx-add:
 	kubectl apply -f flux/lab-configs/ingress-nginx.yaml
 
@@ -38,3 +41,14 @@ ingress-prep:
 
 ingress-cleanup: ingress-delete ingress-prep-delete
 	-make -s ingress-nginx-delete
+
+
+egress-setup: cilium
+	docker run -d --rm --network kind --name echo mpolden/echoip -l ":80"
+	kubectl apply -f flux/lab-configs/egress.yaml
+	kubectl patch kustomizations -n flux metallb --patch '{"spec": {"postBuild": {"substitute": {"destination_cidr": "$(KIND_BRIDGE_SUBNET)"}}}}' --type=merge
+	kubectl patch kustomizations -n flux metallb --patch '{"spec": {"postBuild": {"substitute": {"egress_ip": "$(CONTROL_PLANE_NODE_IP)"}}}}' --type=merge
+
+egress-cleanup:
+	docker rm -f echo
+	kubectl delete -f flux/lab-configs/egress.yaml
